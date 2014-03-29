9c89373d0aca90d77e3f0af70fea97e04a54b5f3
Alex Ungur <alexaandru@gmail.com>
2014-03-29 11:52:21 +0200

    Overall cleanup, ditched the last global variable

--- go3up.go
+++ go3up.go
@@ -1,17 +1,17 @@
 /*
 Go3Up (Go S3 Uploader) is a small S3 uploader tool.
 
-It was created in order to speed up S3 uploads by employing a little meta data caching about
-what was already uploaded. The tool can remember what files where uploaded (and their md5 sum)
-and on subsequent runs only upload the files that actually changed since the last run.
+It was created in order to speed up S3 uploads by employing a local caching of files md5 sums.
+That way, on subsequent runs, go3up can compute a list of the files that changed since the
+last upload and limit the upload only to those.
 
-The initial use case, was a large static site (with 10k+ files) that needed to change frequently
-only some of the files (about ~100 routinely). In that particular case, the time reduction by
-switching from s3cmd to go3up was quite significant.
+The initial use case was a large static site (with 10k+ files) that frequently changed only
+a small subset of files (about ~100 routinely). In that particular case, the time reduction by
+switching from s3cmd to go3up was significant.
 
 On uploads with empty cache there may not be any benefit.
 
-The current focus of the tool is just one way uploads (without deleting things that were removed
+The current focus of the tool is just one way/uploads (without deleting things that were removed
 locally, yet). That may (or not) change in the future.
 */
 package main
@@ -28,14 +28,19 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
+	"strings"
 	"sync"
 )
 
-// RequestTimeoutError is a frequent error received when there are too many threads.
-// If you encounter this too often, reduce the number of workers.
-const RequestTimeoutError = "Your socket connection to the server was not read from or written to within the timeout period. Idle connections will be closed."
+// Command line options/flags.
+type options struct {
+	workersCount int
+	bucketName, source,
+	cacheFile string
+	dryRun, verbose, quiet, doCache, doUpload bool
+}
 
-// Go3up exit codes
+// Exit codes.
 const (
 	Success = iota
 	CachingFailed
@@ -46,87 +51,91 @@ const (
 	CmdLineOptionError
 )
 
-type options struct {
-	workersCount int
-	bucketName, source,
-	cacheFile string
-	dryRun, verbose, quiet, doCache, doUpload bool
+// S3 errors that we will retry.
+var recoverableErrorsSuffixes = []string{
+	"Idle connections will be closed.",
+	"EOF",
+	"broken pipe",
 }
 
-var auth aws.Auth
+// recoverable verifies if the error given is in recoverableErrorsSuffixes list.
+func recoverable(err error) bool {
+	for _, errSuffix := range recoverableErrorsSuffixes {
+		if strings.HasSuffix(err.Error(), errSuffix) {
+			return true
+		}
+	}
 
-// s3put puts one file to S3 and tries to recover from RequestTimeoutError
-func s3put(bucket *s3.Bucket, fname string, opts *options) (err error) {
+	return false
+}
+
+// quit aborts the program with exitCode, after it prints the error message.
+func quit(msg string, err error, exitCode int) {
+	fmt.Println(msg, err)
+	os.Exit(exitCode)
+}
+
+// display prints either s1 or s2, conditionally.
+func display(cond bool, s1 string, rest ...string) {
+	if cond {
+		fmt.Println(s1)
+	} else if len(rest) > 0 {
+		fmt.Print(rest[0])
+	}
+}
+
+// s3put puts one file to S3 and tries to recover from some errors.
+func s3put(auth aws.Auth, bucket *s3.Bucket, fname string, opts *options) (err error) {
 	fullName := filepath.Join(opts.source, fname)
 	if data, err := ioutil.ReadFile(fullName); err == nil {
 		if err = bucket.Put(fname, data, betterMime(fname), s3.PublicRead); err != nil {
-			if err.Error() == RequestTimeoutError {
-				if opts.verbose || opts.quiet {
-					fmt.Println("Warn: upload failed, retrying:", fname)
-				} else {
-					fmt.Print("r")
-				}
+			if recoverable(err) { // FIXME: Implement exponential backoff.
+				display(opts.verbose || opts.quiet, "Warn: upload failed, retrying: "+fname, "r")
 				bucket = s3.New(auth, aws.EUWest).Bucket(opts.bucketName)
-				return s3put(bucket, fname, opts)
+				return s3put(auth, bucket, fname, opts)
 			}
-
-			fmt.Println("S3 error:", err)
-			os.Exit(S3Failed)
+			quit("S3 error:", err, S3Failed)
 		} else {
-			if opts.verbose {
-				fmt.Println("Uploaded", fname)
-			} else if !opts.quiet {
-				fmt.Print(".")
+			if !opts.quiet {
+				display(opts.verbose, "Uploaded "+fname, ".")
 			}
 		}
 	} else {
-		fmt.Println("Read error:", err)
-		os.Exit(FileReadFailed)
+		quit("Read error:", err, FileReadFailed)
 	}
 
 	return
 }
 
-func uploadWorker(bucket *s3.Bucket, linkChan chan string, wg *sync.WaitGroup, opts *options) {
-	defer wg.Done()
-
-	for fname := range linkChan {
-		if opts.dryRun {
-			if opts.verbose {
-				fmt.Println("Pretending to upload", fname)
-			} else if !opts.quiet {
-				fmt.Print(".")
-			}
-		} else {
-			s3put(bucket, fname, opts)
-		}
-	}
-}
-
-func prepareUploadList(opts *options) (current utils.FileHashes, diff []string) {
+// filesLists returns both the current files list as well as the difference from the old
+// (cached) files list.
+func filesLists(opts *options) (current utils.FileHashes, diff []string) {
 	var old utils.FileHashes
 	current, old = utils.FileHashesNew(opts.source), utils.FileHashes{}
 	old.Load(opts.cacheFile)
 	diff = current.Diff(old)
 
 	if len(diff) == 0 {
-		if !opts.quiet {
-			fmt.Println("Nothing to upload.")
-		}
+		display(!opts.quiet, "Nothing to upload.")
 		os.Exit(Success)
-	} else if opts.verbose {
-		fmt.Printf("List of changes ready, there are %d files to be uploaded to %s:\n", len(diff), opts.bucketName)
+	} else {
+		display(opts.verbose,
+			fmt.Sprintf("List of changes ready, there are %d files to be uploaded to %s:\n", len(diff), opts.bucketName))
 	}
 
 	return
 }
 
-func upload(diff []string, opts *options) {
+// performUpload handles the actual files upload.
+func performUpload(diff []string, opts *options) {
+	var auth aws.Auth
 	var err error
+
 	if auth, err = aws.EnvAuth(); err != nil {
 		fmt.Println(err)
 		os.Exit(S3AuthError)
 	}
+
 	bucket := s3.New(auth, aws.EUWest).Bucket(opts.bucketName)
 
 	wg := new(sync.WaitGroup)
@@ -136,7 +145,19 @@ func upload(diff []string, opts *options) {
 
 	wg.Add(opts.workersCount)
 	for i := 0; i < opts.workersCount; i++ {
-		go uploadWorker(bucket, comm, wg, opts)
+		go func() {
+			defer wg.Done()
+			for fname := range comm {
+				if opts.dryRun {
+					if !opts.quiet {
+						display(opts.verbose, "Pretending to upload "+fname, ".")
+					}
+				} else {
+					s3put(auth, bucket, fname, opts)
+				}
+			}
+		}()
+
 	}
 
 	for _, fname := range diff {
@@ -144,43 +165,39 @@ func upload(diff []string, opts *options) {
 	}
 }
 
-func handleUpload(diff []string, opts *options) {
+// upload handles the upload of changed files.
+func upload(diff []string, opts *options) {
 	if opts.doUpload {
 		if !(opts.quiet || opts.verbose) {
 			fmt.Printf("Uploading to %s ", opts.bucketName)
 		}
 
-		upload(diff, opts)
-
-		if !opts.quiet {
-			fmt.Println("done!")
-		}
-	} else if opts.verbose {
-		fmt.Println("Skipping upload")
+		performUpload(diff, opts)
+		display(!opts.quiet, "done!")
+	} else {
+		display(opts.verbose, "Skipping upload")
 	}
 }
 
-func handleCache(current utils.FileHashes, opts *options) {
+// cache handles the caching of the current list of files.
+func cache(current utils.FileHashes, opts *options) {
 	if opts.doCache {
 		if !opts.dryRun {
 			if err := current.Dump(opts.cacheFile); err != nil {
-				fmt.Println(err)
-				os.Exit(CachingFailed)
+				quit("Caching failed: ", err, CachingFailed)
 			}
-			if opts.verbose {
-				fmt.Println("Updated cache. All done!")
-			}
-		} else if opts.verbose {
-			fmt.Println("Pretending to update cache. All done!")
+			display(opts.verbose, "Updated cache. All done!")
+		} else {
+			display(opts.verbose, "Pretending to update cache. All done!")
 		}
-	} else if opts.verbose {
-		fmt.Println("Skipping cache")
+	} else {
+		display(opts.verbose, "Skipping cache")
 	}
 }
 
+// processCmdLineFlags wraps the command line flags handling.
 func processCmdLineFlags() (opts *options) {
 	opts = new(options)
-
 	flag.IntVar(&opts.workersCount, "workers", 42, "No. of workers/threads to use for S3 uploads")
 	flag.StringVar(&opts.bucketName, "bucket", "", "S3 bucket to upload files to")
 	flag.StringVar(&opts.source, "source", "output", "Source folder for files to be uploaded to S3")
@@ -197,8 +214,9 @@ func processCmdLineFlags() (opts *options) {
 	return
 }
 
+// validateCmdLineFlags validates some of the flags, mostly paths.
+// Defers actual validation to validateCmdLineFlag()
 func validateCmdLineFlags(opts *options) (err error) {
-	// FIXME: More checks: if source is a folder, if hash file is a file, if bucket exists, etc.
 	flags := map[string]string{
 		"Bucket Name": opts.bucketName,
 		"Source":      opts.source,
@@ -215,27 +233,25 @@ func validateCmdLineFlags(opts *options) (err error) {
 	return
 }
 
-// NOTE: currently it is limited to just checking paths (if set and if they exist)
+// validateCmdLineFlag handles the actual validation of flags.
 func validateCmdLineFlag(label, val string) (err error) {
 	switch label {
 	case "Bucket Name":
 		if val == "" {
-			err = errors.New(label + " is not set")
+			return errors.New(label + " is not set")
 		}
-		return
 	default:
 		_, err = os.Stat(val)
-		return
-
 	}
+
+	return
 }
 
+// betterMime wrapps mime.TypeByExtension and tries to handle a few edge cases.
 func betterMime(fname string) (mt string) {
-	ext := filepath.Ext(fname)
+	ext := strings.ToLower(filepath.Ext(fname))
 	if mt = mime.TypeByExtension(ext); mt != "" {
 		return
-	} else if ext == ".JPG" {
-		mt = "image/jpeg"
 	} else if ext == ".ttf" {
 		mt = "binary/octet-stream"
 	}
@@ -243,10 +259,11 @@ func betterMime(fname string) (mt string) {
 	return
 }
 
+// TODO: On unrecoverable error, still cache the ones successfully uploaded.
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	opts := processCmdLineFlags()
-	current, diff := prepareUploadList(opts)
-	handleUpload(diff, opts)
-	handleCache(current, opts)
+	current, diff := filesLists(opts)
+	upload(diff, opts)
+	cache(current, opts)
 }
